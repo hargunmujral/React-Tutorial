534f4c3646b2b9d2adf32321e775f29801f92b0e
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-05-15 16:06:56 -0400

    made a basic booklist

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// stateless functional component
+// always return JSX
 
-function Greeting(){
-  return <h4>this is my first component</h4>
+function BookList(){
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'));
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  )
+};
+const Image = () => 
+  <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1427068429l/23346740.jpg" alt="Sapiens" />
+
+const Title = () => <h1>Sapiens: A Brief History of Humankind </h1>
+const Author = () => <h2>Yuval Noah Harari</h2>
+
+ReactDOM.render(<BookList/>, document.getElementById('root'));
